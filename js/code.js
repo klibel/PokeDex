ec035e873dedfa16229757859e8e5671d042d616
Klibel Romero <klibel.a.romero@gamil.com>
2024-08-21 14:14:09 -0400

    pokedex fine

--- js/code.js
+++ js/code.js
@@ -15,16 +15,14 @@ for (let i = 1; i <= 250; i++) {
 }
 
 function insertPokemonHtml(pokemon) {
-
   let tipe = pokemon.types.map((type) => `<p class="tipe" id="${type.type.name}">${type.type.name}</p>`);
   tipe = tipe.join('');
 
-  let pokemonId = pokemon.id.toString();
-  if (pokemonId.length === 1) {
-    pokemonId = "00" + pokemonId;
-  } else if (pokemonId.length === 2) {
-    pokemonId = "0" + pokemonId;
-  }
+  let pokemonId = pokemon.id.toString().padStart(3, '0');
+
+  // Convertir altura y peso
+  let altura = (pokemon.height / 10).toFixed(1).replace('.', ','); // Convertir a metros y formatear
+  let peso = (pokemon.weight / 10).toFixed(1).replace('.', ','); // Convertir a kilogramos y formatear
 
   const div = document.createElement('div');
   div.classList.add('pokemon-card');
@@ -43,8 +41,8 @@ function insertPokemonHtml(pokemon) {
         ${tipe}
       </div>
       <div class="pokemon-stats">
-        <p class="altura">${pokemon.height}m</p>
-        <p class="peso">${pokemon.weight}Kg</p>
+        <p class="altura">${altura} m</p>
+        <p class="peso">${peso} kg</p>
       </div>
     </div>			
   `;
@@ -73,35 +71,39 @@ function btnFilterPokemon(pokemons) {
   listPokemon.innerHTML = '';
 
   pokemons.forEach(pokemon => {
-    let tipe = pokemon.types.map((type) => `<p class="tipe" id="${type.type.name}"> ${type.type.name} </p>`);
-    tipe = tipe.join('');
+  let tipe = pokemon.types.map((type) => `<p class="tipe" id="${type.type.name}">${type.type.name}</p>`);
+  tipe = tipe.join('');
 
-    let pokemonId = pokemon.id.toString().padStart(3, '0');
-    
-    const div = document.createElement('div');
-    div.classList.add('pokemon-card');
-    div.innerHTML = 
-    `
-      <p class="pokemos-id-back">#${pokemonId}</p>
-      <div class="img-pokemon">
-        <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}">
+  let pokemonId = pokemon.id.toString().padStart(3, '0');
+
+  // Convertir altura y peso
+  let altura = (pokemon.height / 10).toFixed(1).replace('.', ','); // Convertir a metros y formatear
+  let peso = (pokemon.weight / 10).toFixed(1).replace('.', ','); // Convertir a kilogramos y formatear
+
+  const div = document.createElement('div');
+  div.classList.add('pokemon-card');
+  div.innerHTML = 
+  `
+    <p class="pokemos-id-back">#${pokemonId}</p>
+    <div class="img-pokemon">
+      <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}">
+    </div>
+    <div class="pokemon-info">
+      <div class="name-contenedor">
+        <p class="pokemon-id">#${pokemonId}</p>
+        <h2 class="name-pokemon">${pokemon.name}</h2>
       </div>
-      <div class="pokemon-info">
-        <div class="name-contenedor">
-          <p class="pokemon-id">#${pokemonId}</p>
-          <h2 class="name-pokemon">${pokemon.name}</h2>
-        </div>
-        <div class="tipe-pokemon">
-          ${tipe}
-        </div>
-        <div class="pokemon-stats">
-          <p class="altura">${pokemon.height}m</p>
-          <p class="peso">${pokemon.weight}Kg</p>
-        </div>
-      </div>			
-    `;
+      <div class="tipe-pokemon">
+        ${tipe}
+      </div>
+      <div class="pokemon-stats">
+        <p class="altura">${altura} m</p>
+        <p class="peso">${peso} kg</p>
+      </div>
+    </div>			
+  `;
 
-    listPokemon.appendChild(div);
+  listPokemon.appendChild(div);
   });
 }
 
